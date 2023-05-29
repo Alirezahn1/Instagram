d2aa90ec0aa7f7ec95987b62e4561969d9653f1a
alireza <alirezahn1@yahoo.com>
2023-05-29 22:05:18 +0330

    feat Post model

--- posts/models.py
+++ posts/models.py
@@ -26,3 +26,18 @@ class Tag(models.Model):
         if not self.slug:
             self.slug = slugify(self.title)
         return super().save(*args, **kwargs)
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    picture = models.ImageField(upload_to=user_directory_path, verbose_name="Picture")
+    caption = models.CharField(max_length=10000, verbose_name="Caption")
+    posted = models.DateField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag, related_name="tags")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.IntegerField(default=0)
+
+    def get_absolute_url(self):
+        return reverse("post-details", args=[str(self.id)])
+
+    # def __str__(self):
+    #     return str(self.caption)
