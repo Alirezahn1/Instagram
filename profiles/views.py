010a7edcf5004ae43cfacfa5539b0be76e8ddd9a
alireza <alirezahn1@yahoo.com>
2023-05-30 11:10:17 +0330

    fix register view

--- profiles/views.py
+++ profiles/views.py
@@ -5,6 +5,16 @@ from django.views import View
 from .forms import EditProfileForm, UserRegisterForm
 
 class RegisterView(View):
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('posts:home')
+
+    def get(self,request):
+        form = UserRegisterForm()
+        context = {'form': form,}
+        return render(request, 'user/sign-up.html', context)
+
     def post(self,request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
@@ -13,20 +23,8 @@ class RegisterView(View):
             username = form.cleaned_data.get('username')
             messages.success(request, f'Hurray your account was created!!')
 
-            # Automatically Log In The User
             new_user = authenticate(username=form.cleaned_data['username'],
                                     password=form.cleaned_data['password1'], )
             login(request, new_user)
-            # return redirect('editprofile')
-            return redirect('index')
+            return redirect('posts:home')
 
-
-
-    # elif request.user.is_authenticated:
-    #     return redirect('index')
-    def get(self,request):
-        form = UserRegisterForm()
-        context = {
-        'form': form,
-      }
-        return render(request, 'user/sign-up.html', context)
