e2907ef45b64fc12a9b83f03ff146187de17625b
alireza <alirezahn1@yahoo.com>
2023-05-30 11:10:48 +0330

    feat log out url and template

--- profiles/urls.py
+++ profiles/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     # User Authentication
     path('sign-up/', views.RegisterView.as_view(), name="sign-up"),
     path('sign-in/', auth_views.LoginView.as_view(template_name="user/sign-in.html", redirect_authenticated_user=True), name='sign-in'),
-    # path('sign-out/', auth_views.LogoutView.as_view(template_name="sign-out.html"), name='sign-out'),
+    path('sign-out/', auth_views.LogoutView.as_view(template_name="user/sign-out.html"), name='sign-out'),
 ]
