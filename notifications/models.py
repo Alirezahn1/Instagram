6f169ffc2365296b28960346ca42022a71f66dc9
alireza <alirezahn1@yahoo.com>
2023-07-14 13:42:08 +0330

    fix notification model

--- notifications/models.py
+++ notifications/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-
 from django.contrib.auth.models import User
-from posts.models import Post
+# from post.models import Post
 
 class Notification(models.Model):
     NOTIFICATION_TYPES = ((1, 'Like'), (2, 'Comment'), (3, 'Follow'))
 
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="notification_post", null=True)
+    post = models.ForeignKey("posts.Post", on_delete=models.CASCADE, related_name="notification_post", null=True)
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notification_from_user" )
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notification_to_user" )
     notification_types = models.IntegerField(choices=NOTIFICATION_TYPES, null=True, blank=True)
@@ -14,5 +13,5 @@ class Notification(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     is_seen = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.text_preview
+    # def __str__(self):
+    #     return self.text_preview
