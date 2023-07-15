f0d42f9a5af69e8867871631d28209d47a5a268e
alireza <alirezahn1@yahoo.com>
2023-07-15 18:05:45 +0330

    feat new conversation url

--- directs/urls.py
+++ directs/urls.py
@@ -1,4 +1,4 @@
-from directs.views import inbox, Directs, SendDirect, UserSearch
+from directs.views import inbox, Directs, SendDirect, UserSearch, NewConversation
 from django.urls import path
 
 app_name = 'directs'
@@ -8,4 +8,5 @@ urlpatterns = [
     path('direct/<username>', Directs, name="directs"),
     path('send/', SendDirect, name="send-directs"),
     path('search/', UserSearch, name="search-users"),
+    path('new/<username>', NewConversation, name="conversation"),
 ]
