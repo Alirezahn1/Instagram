2d427e43e7e53c84abda351bb44b131c4bd50aa2
alireza <alirezahn1@yahoo.com>
2023-07-15 18:26:17 +0330

    feat delete notification template

--- notifications/views.py
+++ notifications/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from notifications.models import Notification
@@ -13,3 +13,8 @@ def ShowNotification(request):
 
     }
     return render(request, 'notification/notification.html', context)
+
+def DeleteNotification(request, noti_id):
+    user = request.user
+    Notification.objects.filter(id=noti_id, user=user).delete()
+    return redirect('show-notification')
