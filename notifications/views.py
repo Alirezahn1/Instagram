dfcf82552e7a443b153b3687a0031db00447321e
alireza <alirezahn1@yahoo.com>
2023-07-15 18:25:10 +0330

    feat show notification view

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from notifications.models import Notification
+
+
+def ShowNotification(request):
+    user = request.user
+    notifications = Notification.objects.filter(user=user).order_by('-date')
+
+    context = {
+        'notifications': notifications,
+
+    }
+    return render(request, 'notification/notification.html', context)
