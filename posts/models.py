999d2fa770a701bc0b5c09fdebc75040f559211f
alireza <alirezahn1@yahoo.com>
2023-07-15 18:16:43 +0330

    fix tag url

--- posts/models.py
+++ posts/models.py
@@ -23,7 +23,7 @@ class Tag(models.Model):
         verbose_name_plural = 'Tags'
 
     def get_absolute_url(self):
-        return reverse('tags', args=[self.slug])
+        return reverse('posts:tags', args=[self.slug])
 
     def __str__(self):
         return self.title
