2a01a730c7b222b50a2469e9fb15296d1eced110
alireza <alirezahn1@yahoo.com>
2023-07-15 15:26:42 +0330

    feat direct url

--- directs/urls.py
+++ directs/urls.py
@@ -1,10 +1,10 @@
-from directs.views import inbox
+from directs.views import inbox, Directs
 from django.urls import path
 
 app_name = 'directs'
 
 urlpatterns = [
     path('inbox', inbox, name="message"),
-
+    path('direct/<username>', Directs, name="directs"),
 
 ]
